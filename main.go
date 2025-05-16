c84dbfdf7a6a700ec966bdd50013cfdedbc704d6
Fariha Promi <farihapromi2611@gmail.com>
2025-05-16 10:54:44 +0600

    Add variable

---
+++ main.go
@@ -0,0 +1,11 @@
+// The first statement of go must be package
+package main
+
+import "fmt" //for print function
+
+func main() {
+	var conferenceName = "Go to Conference"
+	fmt.Println("Welcome to our", conferenceName, " booking application")
+	fmt.Println("Get your tickets to attend")
+
+}
