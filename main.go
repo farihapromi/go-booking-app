910d4b76ea7bababe6bc12bbd99535749a127837
Fariha Promi <farihapromi2611@gmail.com>
2025-05-16 20:24:21 +0600

    Add slice method for dynamic

--- main.go
+++ main.go
@@ -1,29 +1,50 @@
-// The first statement of go must be package
 package main
 
-import "fmt" //for print function
+import "fmt"
 
 func main() {
 	var conferenceName = "Go to Conference"
 	const conferenceTickets = 50
-	var remainingTickets = 46
-	//type print
-	fmt.Printf("conferenct Tickets is %T\n", conferenceTickets)
+	var remainingTickets uint = 46
 
-	fmt.Println("Welcome to our", conferenceName, " booking application")
-	fmt.Println("we have total tickets", conferenceTickets, "Remaining ticket", remainingTickets)
+	// Print types
+	fmt.Printf("conferenceTickets is of type: %T\n", conferenceTickets)
+
+	fmt.Println("Welcome to our", conferenceName, "booking application")
+	fmt.Println("We have total tickets:", conferenceTickets, "Remaining tickets:", remainingTickets)
 	fmt.Println("Get your tickets to attend")
-	//data types
+
+	// Input data
 	var userName string
-	var userTickets int
+	var lastName string
+	var userTickets uint
 	var email string
-	fmt.Println("Enter user name")
+
+	// Read user input
+	fmt.Println("Enter first name:")
 	fmt.Scan(&userName)
-	fmt.Println("Enter user email")
+
+	fmt.Println("Enter last name:")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter email:")
 	fmt.Scan(&email)
-	fmt.Println("Enter user tickets")
+
+	fmt.Println("Enter number of tickets:")
 	fmt.Scan(&userTickets)
 
-	fmt.Printf("Thank you  %v booked %v tickets .\n.Your confirmation will be recieved at %v email", userName, userTickets, email)
+	// Update remaining tickets
+	remainingTickets = remainingTickets - userTickets
 
+	// Confirmation message
+	fmt.Printf("Thank you %v %v for booking %v tickets.\n", userName, lastName, userTickets)
+	fmt.Printf("You will receive confirmation at %v\n", email)
+	fmt.Printf("Tickets remaining: %v\n", remainingTickets)
+
+	// Append to bookings slice
+	var bookings []string
+	bookings = append(bookings, userName+" "+lastName)
+
+	fmt.Printf("Bookings: %v\n", bookings)
+	fmt.Printf("Number of bookings: %v\n", len(bookings))
 }
